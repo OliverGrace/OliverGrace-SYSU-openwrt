9fe713d56c7c34436a1acee98e161b36fba822ed
OliverGrace <1328780068@qq.com>
2021-05-29 13:51:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,7 @@ git clone git clone https://bitbucket.org/etnperlong/luci-app-mentohust-sysu.git
 git clone git clone https://bitbucket.org/etnperlong/luci-app-mentohust-sysu.git
 git clone https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/kenzok8/small.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-vssr.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 cd ..
 cd ..
